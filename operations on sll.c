73bd2cc210d64653395774f7bc82b4a7cc3a31a5
Khushi <55304030+Kjp04@users.noreply.github.com>
2021-11-17 19:44:20 +0530

    Functions and operations on singly linked list

---
+++ operations on sll.c
@@ -0,0 +1,210 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node{
+	int data;
+	struct node *next;
+};
+struct node *head;
+
+
+void create(){
+    struct node *temp,*newnode;
+int i,n,ch;
+// scanf("%d",&n);
+   while(ch){
+
+newnode= (struct  node*)malloc(sizeof(struct node));
+printf("\n Enter the data \n");
+scanf("%d",&newnode->data);
+
+newnode->next=NULL;
+if(head==0){
+    temp=head=newnode;
+}
+	else{
+	    
+	    
+	   
+	    temp->next=newnode;
+	    temp=newnode;
+	}
+printf("Do you wish to continue [1/0]: ");
+        scanf("%d", &ch);
+	
+}
+}
+
+
+void reverse(){
+	struct node *temp, *prev=NULL, *next=NULL;
+        temp=head;
+        while (temp != NULL)
+            {
+            next = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = next;
+            }
+        head = prev;
+        printf("reverse string is:\n");
+        while(head!=NULL)
+        {
+            printf("%d ",head->data);
+            head=head->next;
+        }
+}
+
+
+void display()
+{        struct node *temp;
+     if(head==NULL)
+        {
+                printf("\n Linked List is empty:\n");
+                return;
+        }
+  else{
+      temp=head;
+    while (temp != 0) {
+        printf("%d", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+  }
+}
+
+void insert_begin(){
+    struct node *newnode;
+    newnode= (struct  node*)malloc(sizeof(struct node));
+printf("\n Enter the data \n");
+scanf("%d",&newnode->data);
+if(head==0){
+    head=newnode;
+}
+else{
+    newnode->next =head;
+    head=newnode;
+}
+}
+
+
+void insert_end(){
+    struct node *newnode,*temp;
+    temp=head;
+    newnode= (struct  node*)malloc(sizeof(struct node));
+printf("\n Enter the data \n");
+scanf("%d",&newnode->data);
+while(temp->next!=0){
+    temp=temp->next;
+}
+temp->next= newnode;
+}
+
+
+void insert_pos(){
+    struct node *newnode,*temp;
+    int i=1, pos,count = 0;
+    temp=head;
+    newnode= (struct  node*)malloc(sizeof(struct node));
+printf("\n Enter the data \n");
+scanf("%d",&newnode->data);
+printf("Enter position");
+scanf("%d",&pos);
+while(temp!=0){
+    count++;
+    temp= temp->next;
+}
+if(pos>count || pos<0){
+    printf("Invalid position");
+}
+else {
+    while(i<pos-1){
+        temp= temp->next;
+        i++;
+    }
+    newnode->next = temp->next;
+    temp->next =newnode;
+}
+}
+
+void delete_begin(){
+    
+}
+
+
+void delete_end(){
+    
+}
+
+
+void delete_pos(){
+    
+}
+
+int main(){
+int choice;
+while(1){
+    printf("\n Welcome to operations on Singly linked list \n");
+    printf("1. Create \n");
+    printf("2. Display\n");
+    printf("3. Insert at beginning\n");
+    printf("4. Insert at end\n");
+    printf("5. Insert at specific position\n");
+    printf("6. Reverse the list\n");
+    printf("7. Delete from beginning\n");
+    printf("8. Delete from end\n");
+    printf("9. Delete from specific position\n");
+    printf("Enter choice");
+    scanf("%d",&choice);
+    switch(choice)
+                {
+                        case 1:
+                                        create();
+                                        break;
+                        case 2:
+                                        display();
+                                        break;
+                        case 3:
+                                        insert_begin();
+                                        break;
+                        case 4:
+                                        insert_end();
+                                        break;
+                        case 5:
+                                        insert_pos();
+                                        break;
+                        case 6:
+                                        reverse();
+                                        break;
+                        case 7:
+                                        delete_begin();
+                                        break;
+                        case 8:
+                                        delete_end();
+                                        break;
+                        case 9:
+                                        delete_pos();
+                                        break;
+                        case 10:
+                                        exit(0);
+                                        break;
+
+                        default:
+                                        printf("\n Wrong Choice:\n");
+                                        break;
+                }
+}
+return 0;
+   
+}
+
